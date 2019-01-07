a5f51528c2342b05e3ba76d538a7a07e58b6cfc1
ruima027 <ruima027@gmail.com>
2019-01-08 01:58:02 +0800

    Git second commit

--- src/components/counts/counters.jsx
+++ src/components/counts/counters.jsx
@@ -5,11 +5,11 @@ import Counter from './counter'
 class Counters extends Component{
     state = {
         counters: [
-            {id:1, value:1},
-            {id:2, value:2},
-            {id:3, value:3},
-            {id:4, value:4},
-            {id:5, value:5}
+            {id:1, value:10},
+            {id:2, value:20},
+            {id:3, value:30},
+            {id:4, value:40},
+            {id:5, value:50}
         ]
     }
     handledelete = counterID => {
