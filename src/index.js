fea2337c2ad3fe4e7752cc79e3f062487f6a353f
ruima027 <ruima027@gmail.com>
2019-01-22 18:57:55 +0800

    for-client

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+import 'react-app-polyfill/ie9'; // For IE 9-11 support
+import 'react-app-polyfill/ie11'; // For IE 11 support
+import './polyfill'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Counters from './components/counts/counters'
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const element =<h1 className='alert alert-danger'>Hello world</h1>
-ReactDOM.render(<Counters/>,document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
